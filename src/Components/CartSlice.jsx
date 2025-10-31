37bab3bac6bcbfd1aa7d895aa358e9c4c338bc39
ycjuliana <ycjuliana@gmail.com>
2025-10-31 16:01:05 -0400

    initial commit

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -1,6 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
 
-const CartSlice = ({
- 
-});
+const initialState = {
+    cartItems: [],
+  };
+
+  const CartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+    }
+  });
 
 
